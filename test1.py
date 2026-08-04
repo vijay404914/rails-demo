6de6d4c05f5be09e164374d92259a676404f759b
vijay sahu <vijaysahu4914@gmail.com>
2026-08-04 17:00:51 +0530

    demo

--- test1.py
+++ test1.py
@@ -1,8 +1,2 @@
-print 'hi vijay i am ruby on rails developer'
-print 'ruby on rails easy to learn '
-rpiin ''sdfsdfsd
-prin shbds
-fdsfsdf
-dfhsk 
-fsh dfiu 
-wduf hiu
+print("hi vijay i am ruby on rails developer")
+print("ruby on rails easy to learn")
